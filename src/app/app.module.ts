d31379bcf1d2d8b32c4a29320d72eb5b7f9e9854
Stephen Moreira <smoreira1@live.com>
2018-08-20 19:56:54 -0400

    Veda navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { NgMatModule } from './shared/modules/ngMaterial/ngMaterial.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgMatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/modules/ngMaterial/ngMaterial.module.ts
@@ -0,0 +1,110 @@
+// Base angular imports
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+// Angular Material Imports
+import { MatToolbarModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { MatExpansionModule } from '@angular/material';
+import { MatListModule } from '@angular/material';
+import { MatCardModule } from '@angular/material';
+import { MatGridListModule } from '@angular/material';
+import { MatTabsModule } from '@angular/material';
+import { MatSidenavModule } from '@angular/material';
+import { MatChipsModule } from '@angular/material';
+import { MatTooltipModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatPaginatorModule, MatSortModule } from '@angular/material';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatBadgeModule } from '@angular/material/badge';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+    MatInputModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatListModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatGridListModule,
+    MatTabsModule,
+    MatChipsModule,
+    MatTooltipModule,
+    MatSelectModule,
+    MatProgressBarModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatSlideToggleModule,
+    MatButtonToggleModule,
+    MatStepperModule,
+    MatAutocompleteModule,
+    MatBottomSheetModule,
+    MatBadgeModule,
+    MatMenuModule,
+    MatSnackBarModule
+  ],
+  providers: [
+  ],
+  bootstrap: [],
+  schemas: [],
+  exports: [    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+    MatInputModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatListModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatGridListModule,
+    MatTabsModule,
+    MatChipsModule,
+    MatTooltipModule,
+    MatSelectModule,
+    MatProgressBarModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatSlideToggleModule,
+    MatButtonToggleModule,
+    MatStepperModule,
+    MatAutocompleteModule,
+    MatBottomSheetModule,
+    MatBadgeModule,
+    MatCheckboxModule,
+    MatMenuModule],
+  entryComponents: []
+})
+
+
+export class NgMatModule {
+
+}
